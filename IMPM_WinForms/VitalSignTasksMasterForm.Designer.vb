bc495e02a7b7a8fad1387af9876f4da5b0f076e8
SDMiller <Scott_Miller@nps.gov>
2018-10-23 16:14:45 -0800

    Got the AssignedTo dropdown working in the VS tasks master form

--- IMPM_WinForms/VitalSignTasksMasterForm.Designer.vb
+++ IMPM_WinForms/VitalSignTasksMasterForm.Designer.vb
@@ -23,31 +23,22 @@ Partial Class VitalSignTasksMasterForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim TblVitalSignTasksGridEX_DesignTimeLayout As Janus.Windows.GridEX.GridEXLayout = New Janus.Windows.GridEX.GridEXLayout()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(VitalSignTasksMasterForm))
-        Dim VwVitalSignWorkLogGridEX_DesignTimeLayout As Janus.Windows.GridEX.GridEXLayout = New Janus.Windows.GridEX.GridEXLayout()
         Me.AKRODataSet = New IMPM.AKRODataSet()
-        Me.VwVitalSignWorkLogBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.VwVitalSignWorkLogTableAdapter = New IMPM.AKRODataSetTableAdapters.vwVitalSignWorkLogTableAdapter()
+        Me.TblVitalSignTasksBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblVitalSignTasksTableAdapter = New IMPM.AKRODataSetTableAdapters.tblVitalSignTasksTableAdapter()
         Me.TableAdapterManager = New IMPM.AKRODataSetTableAdapters.TableAdapterManager()
-        Me.VwVitalSignWorkLogBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
-        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
-        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
-        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
-        Me.VwVitalSignWorkLogGridEX = New Janus.Windows.GridEX.GridEX()
+        Me.TblVitalSignTasksGridEX = New Janus.Windows.GridEX.GridEX()
+        Me.TblVitalSignsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblVitalSignsTableAdapter = New IMPM.AKRODataSetTableAdapters.tblVitalSignsTableAdapter()
+        Me.TblContactsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblContactsTableAdapter = New IMPM.AKRODataSetTableAdapters.tblContactsTableAdapter()
         CType(Me.AKRODataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.VwVitalSignWorkLogBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.VwVitalSignWorkLogBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.VwVitalSignWorkLogBindingNavigator.SuspendLayout()
-        CType(Me.VwVitalSignWorkLogGridEX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblVitalSignTasksBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblVitalSignTasksGridEX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblVitalSignsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblContactsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'AKRODataSet
@@ -55,21 +46,20 @@ Partial Class VitalSignTasksMasterForm
         Me.AKRODataSet.DataSetName = "AKRODataSet"
         Me.AKRODataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
-        'VwVitalSignWorkLogBindingSource
+        'TblVitalSignTasksBindingSource
         '
-        Me.VwVitalSignWorkLogBindingSource.DataMember = "vwVitalSignWorkLog"
-        Me.VwVitalSignWorkLogBindingSource.DataSource = Me.AKRODataSet
+        Me.TblVitalSignTasksBindingSource.DataMember = "tblVitalSignTasks"
+        Me.TblVitalSignTasksBindingSource.DataSource = Me.AKRODataSet
         '
-        'VwVitalSignWorkLogTableAdapter
+        'TblVitalSignTasksTableAdapter
         '
-        Me.VwVitalSignWorkLogTableAdapter.ClearBeforeFill = True
+        Me.TblVitalSignTasksTableAdapter.ClearBeforeFill = True
         '
         'TableAdapterManager
         '
         Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
-        Me.TableAdapterManager.Connection = Nothing
         Me.TableAdapterManager.DataManagementMilestonesTableAdapter = Nothing
-        Me.TableAdapterManager.tblContactsTableAdapter = Nothing
+        Me.TableAdapterManager.tblContactsTableAdapter = Me.TblContactsTableAdapter
         Me.TableAdapterManager.tblNetworksTableAdapter = Nothing
         Me.TableAdapterManager.tblNetworkTasksTableAdapter = Nothing
         Me.TableAdapterManager.tblProtocolDeliverablesTableAdapter = Nothing
@@ -77,170 +67,65 @@ Partial Class VitalSignTasksMasterForm
         Me.TableAdapterManager.tblVitalSignDataManagementSummaryTableAdapter = Nothing
         Me.TableAdapterManager.tblVitalSignObjectivesTableAdapter = Nothing
         Me.TableAdapterManager.tblVitalSignProtocolsTableAdapter = Nothing
-        Me.TableAdapterManager.tblVitalSignsTableAdapter = Nothing
-        Me.TableAdapterManager.tblVitalSignTasksTableAdapter = Nothing
+        Me.TableAdapterManager.tblVitalSignsTableAdapter = Me.TblVitalSignsTableAdapter
+        Me.TableAdapterManager.tblVitalSignTasksTableAdapter = Me.TblVitalSignTasksTableAdapter
         Me.TableAdapterManager.tblVitalSignWorkLogTableAdapter = Nothing
         Me.TableAdapterManager.UpdateOrder = IMPM.AKRODataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
         '
-        'VwVitalSignWorkLogBindingNavigator
+        'TblVitalSignTasksGridEX
         '
-        Me.VwVitalSignWorkLogBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
-        Me.VwVitalSignWorkLogBindingNavigator.BindingSource = Me.VwVitalSignWorkLogBindingSource
-        Me.VwVitalSignWorkLogBindingNavigator.CountItem = Me.BindingNavigatorCountItem
-        Me.VwVitalSignWorkLogBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
-        Me.VwVitalSignWorkLogBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.VwVitalSignWorkLogBindingNavigatorSaveItem})
-        Me.VwVitalSignWorkLogBindingNavigator.Location = New System.Drawing.Point(0, 0)
-        Me.VwVitalSignWorkLogBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
-        Me.VwVitalSignWorkLogBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
-        Me.VwVitalSignWorkLogBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
-        Me.VwVitalSignWorkLogBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
-        Me.VwVitalSignWorkLogBindingNavigator.Name = "VwVitalSignWorkLogBindingNavigator"
-        Me.VwVitalSignWorkLogBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
-        Me.VwVitalSignWorkLogBindingNavigator.Size = New System.Drawing.Size(1184, 25)
-        Me.VwVitalSignWorkLogBindingNavigator.TabIndex = 0
-        Me.VwVitalSignWorkLogBindingNavigator.Text = "BindingNavigator1"
+        Me.TblVitalSignTasksGridEX.DataSource = Me.TblVitalSignTasksBindingSource
+        TblVitalSignTasksGridEX_DesignTimeLayout.LayoutString = resources.GetString("TblVitalSignTasksGridEX_DesignTimeLayout.LayoutString")
+        Me.TblVitalSignTasksGridEX.DesignTimeLayout = TblVitalSignTasksGridEX_DesignTimeLayout
+        Me.TblVitalSignTasksGridEX.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TblVitalSignTasksGridEX.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TblVitalSignTasksGridEX.Location = New System.Drawing.Point(0, 0)
+        Me.TblVitalSignTasksGridEX.Name = "TblVitalSignTasksGridEX"
+        Me.TblVitalSignTasksGridEX.Size = New System.Drawing.Size(1342, 724)
+        Me.TblVitalSignTasksGridEX.TabIndex = 1
         '
-        'BindingNavigatorMoveFirstItem
+        'TblVitalSignsBindingSource
         '
-        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
-        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        Me.TblVitalSignsBindingSource.DataMember = "tblVitalSigns"
+        Me.TblVitalSignsBindingSource.DataSource = Me.AKRODataSet
         '
-        'BindingNavigatorMovePreviousItem
+        'TblVitalSignsTableAdapter
         '
-        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
-        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        Me.TblVitalSignsTableAdapter.ClearBeforeFill = True
         '
-        'BindingNavigatorSeparator
+        'TblContactsBindingSource
         '
-        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        Me.TblContactsBindingSource.DataMember = "tblContacts"
+        Me.TblContactsBindingSource.DataSource = Me.AKRODataSet
         '
-        'BindingNavigatorPositionItem
+        'TblContactsTableAdapter
         '
-        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
-        Me.BindingNavigatorPositionItem.AutoSize = False
-        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
-        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
-        Me.BindingNavigatorPositionItem.Text = "0"
-        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
-        '
-        'BindingNavigatorCountItem
-        '
-        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
-        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 15)
-        Me.BindingNavigatorCountItem.Text = "of {0}"
-        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
-        '
-        'BindingNavigatorSeparator1
-        '
-        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
-        '
-        'BindingNavigatorMoveNextItem
-        '
-        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
-        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorMoveNextItem.Text = "Move next"
-        '
-        'BindingNavigatorMoveLastItem
-        '
-        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
-        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorMoveLastItem.Text = "Move last"
-        '
-        'BindingNavigatorSeparator2
-        '
-        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
-        '
-        'BindingNavigatorAddNewItem
-        '
-        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
-        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorAddNewItem.Text = "Add new"
-        '
-        'BindingNavigatorDeleteItem
-        '
-        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
-        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorDeleteItem.Text = "Delete"
-        '
-        'VwVitalSignWorkLogBindingNavigatorSaveItem
-        '
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem.Enabled = False
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem.Image = CType(resources.GetObject("VwVitalSignWorkLogBindingNavigatorSaveItem.Image"), System.Drawing.Image)
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem.Name = "VwVitalSignWorkLogBindingNavigatorSaveItem"
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 20)
-        Me.VwVitalSignWorkLogBindingNavigatorSaveItem.Text = "Save Data"
-        '
-        'VwVitalSignWorkLogGridEX
-        '
-        Me.VwVitalSignWorkLogGridEX.DataSource = Me.VwVitalSignWorkLogBindingSource
-        VwVitalSignWorkLogGridEX_DesignTimeLayout.LayoutString = resources.GetString("VwVitalSignWorkLogGridEX_DesignTimeLayout.LayoutString")
-        Me.VwVitalSignWorkLogGridEX.DesignTimeLayout = VwVitalSignWorkLogGridEX_DesignTimeLayout
-        Me.VwVitalSignWorkLogGridEX.Location = New System.Drawing.Point(62, 64)
-        Me.VwVitalSignWorkLogGridEX.Name = "VwVitalSignWorkLogGridEX"
-        Me.VwVitalSignWorkLogGridEX.Size = New System.Drawing.Size(1092, 545)
-        Me.VwVitalSignWorkLogGridEX.TabIndex = 1
+        Me.TblContactsTableAdapter.ClearBeforeFill = True
         '
         'VitalSignTasksMasterForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1184, 761)
-        Me.Controls.Add(Me.VwVitalSignWorkLogGridEX)
-        Me.Controls.Add(Me.VwVitalSignWorkLogBindingNavigator)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.ClientSize = New System.Drawing.Size(1342, 724)
+        Me.Controls.Add(Me.TblVitalSignTasksGridEX)
         Me.Name = "VitalSignTasksMasterForm"
-        Me.Text = "Vital SignT asks"
+        Me.Text = "VitalSignTasksMasterForm"
         CType(Me.AKRODataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.VwVitalSignWorkLogBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.VwVitalSignWorkLogBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.VwVitalSignWorkLogBindingNavigator.ResumeLayout(False)
-        Me.VwVitalSignWorkLogBindingNavigator.PerformLayout()
-        CType(Me.VwVitalSignWorkLogGridEX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TblVitalSignTasksBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TblVitalSignTasksGridEX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TblVitalSignsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TblContactsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents AKRODataSet As AKRODataSet
-    Friend WithEvents VwVitalSignWorkLogBindingSource As BindingSource
-    Friend WithEvents VwVitalSignWorkLogTableAdapter As AKRODataSetTableAdapters.vwVitalSignWorkLogTableAdapter
+    Friend WithEvents TblVitalSignTasksBindingSource As BindingSource
+    Friend WithEvents TblVitalSignTasksTableAdapter As AKRODataSetTableAdapters.tblVitalSignTasksTableAdapter
     Friend WithEvents TableAdapterManager As AKRODataSetTableAdapters.TableAdapterManager
-    Friend WithEvents VwVitalSignWorkLogBindingNavigator As BindingNavigator
-    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
-    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
-    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
-    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
-    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
-    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
-    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
-    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
-    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
-    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
-    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
-    Friend WithEvents VwVitalSignWorkLogBindingNavigatorSaveItem As ToolStripButton
-    Friend WithEvents VwVitalSignWorkLogGridEX As Janus.Windows.GridEX.GridEX
+    Friend WithEvents TblVitalSignTasksGridEX As Janus.Windows.GridEX.GridEX
+    Friend WithEvents TblVitalSignsTableAdapter As AKRODataSetTableAdapters.tblVitalSignsTableAdapter
+    Friend WithEvents TblVitalSignsBindingSource As BindingSource
+    Friend WithEvents TblContactsTableAdapter As AKRODataSetTableAdapters.tblContactsTableAdapter
+    Friend WithEvents TblContactsBindingSource As BindingSource
 End Class
--- IMPM_WinForms/VitalSignTasksMasterForm.vb
+++ IMPM_WinForms/VitalSignTasksMasterForm.vb
@@ -1,7 +1,24 @@
 ﻿Public Class VitalSignTasksMasterForm
+    Private Sub TblVitalSignTasksBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+        Me.Validate()
+        Me.TblVitalSignTasksBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.AKRODataSet)
+
+    End Sub
+
     Private Sub VitalSignTasksMasterForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'AKRODataSet.vwVitalSignWorkLog' table. You can move, or remove it, as needed.
-        Me.VwVitalSignWorkLogTableAdapter.Fill(Me.AKRODataSet.vwVitalSignWorkLog)
+        'TODO: This line of code loads data into the 'AKRODataSet.tblContacts' table. You can move, or remove it, as needed.
+        Me.TblContactsTableAdapter.Fill(Me.AKRODataSet.tblContacts)
+        'load data from database
+        Me.TblVitalSignsTableAdapter.Fill(Me.AKRODataSet.tblVitalSigns)
+        Me.TblVitalSignTasksTableAdapter.Fill(Me.AKRODataSet.tblVitalSignTasks)
+
+        'set up ContactsDropDown using the GridEX designer, then set the data binding in code here to retrieve the records
+        Try
+            Me.TblVitalSignTasksGridEX.DropDowns("ContactsDropDown").SetDataBinding(AKRODataSet, "tblContacts")
+        Catch ex As Exception
+            MsgBox(ex.Message & " " & System.Reflection.MethodBase.GetCurrentMethod.Name)
+        End Try
 
     End Sub
 End Class
